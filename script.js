66c4fa83da13dc7d624314011df395e259858a0a
corymfonteyne <corymfonteyne@gmail.com>
2022-07-01 16:38:40 -0400

    final

---
+++ script.js
@@ -0,0 +1,40 @@
+let movieTitle1 = "The Godfather";
+let movieTitle2 = "The Shawshank Redemtion";
+let movieTitle3 = "Schindler's List";
+let movieTitle4 = "Raging Bull";
+let movieTitle5 = "Casablanca";
+let movieTitle6 = "Citizen Kane";
+let movieTitle7 = "Gone with the Wind";
+let movieTitle8 = "The Wizard of Oz";
+let movieTitle9 = "One Flew Over the Cuckoo's Nest";
+let movieTitle10 = "Lawrence of Arabia";
+let movieDirector1 ="Francis Ford Coppola";
+let movieDirector2 = "Frank Darabont";
+let movieDirector3 = "Steven Spielberg";
+let movieDirector4 = "Martin Scorsese";
+let movieDirector5 = "Michael Curtiz";
+let movieDirector6 = "Orson Welles";
+let movieDirector7 = "Victor Fleming";
+let movieDirector8 = "Victor Fleming";  
+let movieDirector9 = "Milos Forman";  
+let movieDirector10 = "David Lean";
+let uName = "Cory";
+let uEmail = "corymfonteyne@gmail.com";
+let rating = "5 stars";
+let movieLength;
+let album;
+let related;
+let radioStation;
+
+document.write(`
+<p>${movieTitle1} by ${movieDirector1}</p>
+<p>${movieTitle2} by ${movieDirector2}</p>
+<p>${movieTitle3} by ${movieDirector3}</p> 
+<p>${movieTitle4} by ${movieDirector4}</p>
+<p>${movieTitle5} by ${movieDirector5}</p> 
+<p>${movieTitle6} by ${movieDirector6}</p>
+<p>${movieTitle7} by ${movieDirector7}</p> 
+<p>${movieTitle8} by ${movieDirector8}</p> 
+<p>${movieTitle9} by ${movieDirector9}</p> 
+<p>${movieTitle10} by ${movieDirector10}</p>
+`)
